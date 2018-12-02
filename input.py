56bade96013841166a8fda1b4532b846dbc9db14
tadowdy <tadowdy@siue.edu>
2018-12-02 15:41:37 -0600

    unzip dataset folder and read in data into a dictionary

--- input.py
+++ input.py
@@ -1,2 +1,12 @@
 # date:     11/22/2018
 # purpose:  for reading and storing the input data
+
+import json
+
+inputstr = ""
+with open("SampleDataset1/SampleDataset1.json", 'r') as file:
+    inputstr = file.read()
+
+json_str = json.loads(inputstr)
+
+print(json_str)
