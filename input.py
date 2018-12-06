b87bfc56725e259d4a4a8f8d3ba87b0590f5267f
tadowdy <tadowdy@siue.edu>
2018-12-05 20:06:47 -0600

    add node and graph classes

--- input.py
+++ input.py
@@ -2,11 +2,32 @@
 # purpose:  for reading and storing the input data
 
 import json
+import sys
 
-inputstr = ""
-with open("SampleDataset1/SampleDataset1.json", 'r') as file:
-    inputstr = file.read()
 
-json_str = json.loads(inputstr)
+class Node:
+    __init__(self, val):
+        self.val
+        self.edges = []
 
-print(json_str)
+
+class Graph:
+    def __init__(self, nodes=[]):
+        self.nodes = nodes  # init a list of nodes
+
+    def add_node(self, node):
+        new_n = Node(node)  # create a new node
+        self.nodes.append(new_n)  # add the node to the list
+
+    def add_edge(self, n1, n2):
+        # add both nodes to the list of edges for each node
+        n1.edges.append(n2)
+        n2.edges.append(n1)
+
+
+def read_input():
+    inputstr = ""  # init str
+    with open("SampleDataset1/SampleDataset1.json", 'r') as file:
+        inputstr = file.read()  # read in the file
+
+    json_str = json.loads(inputstr)  # load in the json
