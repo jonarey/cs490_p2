785d5164552b4829f4f746fa5044d2347689321d
jonarey <jack.reynolds96@gmail.com>
2018-12-06 23:37:06 -0600

    implemented dfs

--- input.py
+++ input.py
@@ -3,6 +3,7 @@
 
 import json
 import sys
+from collections import defaultdict 
 
 controllers = []
 json_data = ""
@@ -24,16 +25,29 @@ class Edge:
 class Graph:
     def __init__(self, edges=[]):
         self.edges = edges  # init a list of nodes
+        self.graph = defaultdict(list)
 
-    def add_node(self, node):
-        new_n = Node(node)  # create a new node
-        self.nodes.append(new_n)  # add the node to the list
+#    def add_node(self, node):
+#        new_n = Node(node)  # create a new node
+#        self.nodes.append(new_n)  # add the node to the list
 
-    def add_edge(self, new_e):
+    def add_edge(self, start, end):
         # add both nodes to the list of edges for each node
-        self.edges.append(new_e)
-        #n2.edges.append(n1)
+        self.graph[start].append(end)
 
+    def dfs_travel(self,start,visited):
+        #mark curr as visited
+        visited[start] = True
+        print start,
+        for i in self.graph[start]:
+            #if not visited search 
+            if visited[i] == False:
+                self.dfs_travel(i,visited)
+
+    def dfs(self,start):
+        #create array of false visited for graph
+        visited = [False]*(len(self.graph))
+        self.dfs_travel(start,visited)
 
 def read_input():
     inputstr = "SampleDataset1/SampleDataset1.json"  # init str
@@ -49,15 +63,16 @@ def read_input():
 # read_input()
 
 # create the graph
-graph = Graph()
+json_graph = Graph()
 
 infile = open("SampleDataset1/SampleDataset1.json", 'r')
 stuff = json.load(infile)
 for row in stuff['rows']:
     # add the edges
-    new_edge = Edge(row['viaGlobalId'], row['fromGlobalId'], row['toGlobalId'])
+    # new_edge = Edge(row['viaGlobalId'], row['fromGlobalId'], row['toGlobalId'])
+    json_graph.add_edge(row['fromGlobalId'], row['toGlobalId'])
     # new_edge.end = row['toGlobalId']
     # new_edge.start = row['fromGlobalId']
-    graph.add_edge(new_edge)
-    print(new_edge.val + " " + new_edge.startid + " " + new_edge.endid)
-
+    #graph.add_edge(new_edge)
+    #print(new_edge.val + " " + new_edge.startid + " " + new_edge.endid)
+json_graph.dfs("B6FA6D95-13A4-48BE-8712-A6C3068DDF57")
